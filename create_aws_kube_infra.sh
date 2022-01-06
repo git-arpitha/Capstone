2eac9361c47f63df55ba820e53861afa3dd23cb8
git-arpitha <93966656+git-arpitha@users.noreply.github.com>
2022-01-06 22:41:55 +0530

    Update create_aws_kube_infra.sh

--- create_aws_kube_infra.sh
+++ create_aws_kube_infra.sh
@@ -4,5 +4,5 @@ if [[ $count -gt 0 ]]
     echo "Skipping Cluster creation .. Infrastructure Exists"
     else
     echo "Creatiing AWS Kubernetes cluster"
-    eksctl create cluster --name capstone --version 1.19 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
+    eksctl create cluster --name capstone --version 1.21 --region $AWS_DEFAULT_REGION --nodegroup-name capstone --node-type t3.large --nodes-min 2 --nodes-max 4 --managed
 fi
